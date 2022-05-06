be7af4372c82294e86c8e3803b748fd27dbf81ef
Floyd Hightower <floyd.hightower27@gmail.com>
2022-05-06 04:49:16 -0400

    Change prints to logging

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
+import logging
 
 from flask import Flask
+from google.cloud import language_v1
 
 app = Flask(__name__)
 
-from google.cloud import language_v1
 
-
-def sample_analyze_entities(text_content):
+def sample_analyze_entities(text_content: str):
     """
     Analyzing Entities in a String
 
@@ -16,8 +16,6 @@ def sample_analyze_entities(text_content):
     """
 
     client = language_v1.LanguageServiceClient()
-
-    # text_content = 'California is a state.'
 
     # Available types: PLAIN_TEXT, HTML
     type_ = language_v1.Document.Type.PLAIN_TEXT
@@ -35,39 +33,39 @@ def sample_analyze_entities(text_content):
 
     # Loop through entitites returned from the API
     for entity in response.entities:
-        print(u"Representative name for the entity: {}".format(entity.name))
+        logging.info(u"Representative name for the entity: {}".format(entity.name))
 
         # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
-        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+        logging.info(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
 
         # Get the salience score associated with the entity in the [0, 1.0] range
-        print(u"Salience score: {}".format(entity.salience))
+        logging.info(u"Salience score: {}".format(entity.salience))
 
         # Loop over the metadata associated with entity. For many known entities,
         # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
         # Some entity types may have additional metadata, e.g. ADDRESS entities
         # may have metadata for the address street_name, postal_code, et al.
         for metadata_name, metadata_value in entity.metadata.items():
-            print(u"{}: {}".format(metadata_name, metadata_value))
+            logging.info(u"{}: {}".format(metadata_name, metadata_value))
 
         # Loop over the mentions of this entity in the input document.
         # The API currently supports proper noun mentions.
         for mention in entity.mentions:
-            print(u"Mention text: {}".format(mention.text.content))
+            logging.info(u"Mention text: {}".format(mention.text.content))
 
             # Get the mention type, e.g. PROPER for proper noun
-            print(
+            logging.info(
                 u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
             )
 
     # Get the language of the text, which will be the same as
     # the language specified in the request or, if not specified,
     # the automatically-detected language.
-    print(u"Language of the text: {}".format(response.language))
+    logging.info(u"Language of the text: {}".format(response.language))
 
 
 @app.route("/")
-def hello_world():
+def hello_world(request):
     name = os.environ.get("NAME", "World")
     sample_analyze_entities("when in the course of human events, it becomes necessary for one Joe Biden to ask Russia...")
     return "Hello {}!".format(name)
